4719095a1bd45a6c105f35f02b54459b5207a982
j-giglio <55398564+j-giglio@users.noreply.github.com>
2019-10-28 15:08:53 -0400

    completed

---
+++ src/trip.js
@@ -0,0 +1,11 @@
+const readlineSync = require("readline-sync")
+
+const stu = Number(readlineSync.question("Students: "))
+const teach = Number(readlineSync.question("Teachers: "))
+const capacity = Number(readlineSync.question("Bus capacity: "))
+
+let passengers = stu + teach;
+let buses = passengers / capacity;
+buses = Math.ceil(buses)
+let leftOver = passengers % capacity;
+console.log(buses + "\n bus(es) is (are) needed, with " + leftOver + " on the last bus.")
